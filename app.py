6356194c3a8ce113819b015e7e996d2bd23263e9
parth1609 <115941606+parth1609@users.noreply.github.com>
2024-04-23 02:16:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -58,22 +58,9 @@ Don't hesitate to ask if you have any questions about your order !
   }]
 
 # Display chat context from history on app rerun after login
-if st.session_state["authenticated"]:
-  if st.session_state["username"]:
-    st.success(f"Welcome {st.session_state['username']}")
-  else:
-    st.success("Welcome guest")
-
-  if st.button("Logout"):
-    st.session_state["authenticated"] = False
-    st.session_state["username"] = None  # Clear username as well (optional)
-    st.experimental_rerun()  # Rerun the app to clear UI elements
-
-  for message in st.session_state.context:
-    with st.chat_message(message["role"]):
-      st.markdown(message["content"])
-else:
-  st.error("Not authenticated. Please login to chat with TableTender.")
+for message in st.session_state.context:
+  with st.chat_message(message["role"]):
+    st.markdown(message["content"])
 
 
 # Process and store Query and Response
@@ -99,11 +86,27 @@ def llm_function(usr_msg):
 # Accept user input
 usr_msg = st.chat_input("What's up?")
 
-# Calling the Function when Input is Provided
-if usr_msg:
-  # Displaying the User Message
-  with st.chat_message("user"):
-    st.markdown(usr_msg)
 
-  llm_function(usr_msg)
+if st.session_state["authenticated"]:
+  if st.session_state["username"]:
+    st.success(f"Welcome {st.session_state['username']}")
+  else:
+    st.success("Welcome guest")
+
+  if st.button("Logout"):
+    st.session_state["authenticated"] = False
+    st.session_state["username"] = None  # Clear username as well (optional)
+    st.experimental_rerun()  # Rerun the app to clear UI elements
+  # Accept user input
+  usr_msg = st.chat_input("What's up?")
+
+  # Calling the Function when Input is Provided
+  if usr_msg:
+    # Displaying the User Message
+    with st.chat_message("user"):
+      st.markdown(usr_msg)
+
+    llm_function(usr_msg)
+else:
+  st.error("Not authenticated. Please login to chat with TableTender.")
 
